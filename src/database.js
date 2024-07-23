999810f376d718df2f795e11aadf3aace74f427b
PrideKetu <ketupride1@gmail.com>
2024-07-23 02:26:47 +0100

    recents

---
+++ src/database.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'schoolmanagementsystem'
+}).promise();
+
+module.exports = connection
